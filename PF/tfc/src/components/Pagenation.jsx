2c156fd0dc747d96146a1ea726363ccb86331683
nopespace <nopespace2001@gmail.com>
2022-12-02 17:04:53 -0500

    update pagenation

--- PF/tfc/src/components/Pagenation.jsx
+++ PF/tfc/src/components/Pagenation.jsx
@@ -1,14 +1,17 @@
-import * as React from "react";
+// For Pagenation
+import { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import ReactPaginate from 'react-paginate';
+import SubscriptionBox from './SubscriptionBox';
 
 // const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 function Items({ currentItems }) {
   return (
     <>
       {currentItems.map((item) => (
-        <StudioDisplay
-          image={item.image}
-          name={item.name}
-          address={item.address}
+        <SubscriptionBox 
+        planName="Standard Plan"
+        price="49"
         />
       ))}
     </>
@@ -52,3 +55,5 @@ function PaginatedItems({ items, itemsPerPage }) {
     </>
   );
 }
+
+export default PaginatedItems;
--- PF/tfc/src/pages/Subscriptions.jsx
+++ PF/tfc/src/pages/Subscriptions.jsx
@@ -1,24 +1,52 @@
-import * as React from 'react';
-import Navigation from '../components/Navigation';
-import SearchBar from '../components/SearchBar';
-import SubscriptionBox from '../components/SubscriptionBox';
+import * as React from "react";
+import Navigation from "../components/Navigation";
+import SearchBar from "../components/SearchBar";
+import SubscriptionBox from "../components/SubscriptionBox";
+import PaginatedItems from "../components/Pagenation";
+import axios from "axios";
 
-const Subscriptions = () => {
-return (
-  <>
-    <Navigation />
-    <div className="flex ">
-      <SubscriptionBox 
-        planName="Standard Plan"
-        price="49"
-      />
-      <SubscriptionBox 
-        planName="Premium Plan"
-        price="99"
-      />
-    </div>
-  </>
-);
+class Subscriptions extends React.Component {
+  state = {
+    details: [],
+  };
+
+  componentDidMount() {
+    let data;
+
+    axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/studios/list/?lat=1&lon=11",
+      // TODO: Add the lat and lon here
+    })
+      .then((res) => {
+        data = res.data;
+        this.setState({
+          details: data,
+        });
+        console.log(data);
+      })
+      .catch((err) => {});
+  }
+
+  render() {
+    return (
+      <>
+        <Navigation />
+        details.map(item => {
+            return <SubscriptionBox
+            planName="Standard Plan"
+            price="49"
+            studioName={item.name}
+            studioAddress={item.address}
+            />
+        })
+        {/* <PaginatedItems items={this.details} itemsPerPage={4} />
+        <div className="flex ">
+          <SubscriptionBox planName="Premium Plan" price="99" />
+        </div> */}
+      </>
+    );
+  }
 }
 
 export default Subscriptions;
