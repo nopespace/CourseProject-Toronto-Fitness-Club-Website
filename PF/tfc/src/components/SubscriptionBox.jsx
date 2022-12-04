e0ee7e286efc4659941e5023870cfb4c19227622
nopespace <nopespace2001@gmail.com>
2022-12-03 19:57:38 -0500

    Remove the Dark Mode

--- PF/tfc/src/components/SubscriptionBox.jsx
+++ PF/tfc/src/components/SubscriptionBox.jsx
@@ -7,21 +7,21 @@ const SubscriptionBox = (props) => {
     billingCycle,
   } = props;
   return (
-      <div class="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8">
         <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
           {billingCycle}
         </h5>
-        <div class="flex items-baseline text-gray-900 dark:text-white">
+        <div class="flex items-baseline text-gray-900">
           <span class="text-3xl font-semibold">$</span>
           <span class="text-5xl font-extrabold tracking-tight">{price}</span>
-          <span class="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+          <span class="ml-1 text-xl font-normal text-gray-500">
           </span>
         </div>
         <ul role="list" class="space-y-5 my-7">
           <li class="flex space-x-3">
             <svg
               aria-hidden="true"
-              class="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
+              class="flex-shrink-0 w-5 h-5 text-blue-600"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -33,14 +33,14 @@ const SubscriptionBox = (props) => {
                 clip-rule="evenodd"
               ></path>
             </svg>
-            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+            <span class="text-base font-normal leading-tight text-gray-500 ">
               2 team members
             </span>
           </li>
           <li class="flex space-x-3">
             <svg
               aria-hidden="true"
-              class="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
+              class="flex-shrink-0 w-5 h-5 text-blue-600"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -52,14 +52,14 @@ const SubscriptionBox = (props) => {
                 clip-rule="evenodd"
               ></path>
             </svg>
-            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+            <span class="text-base font-normal leading-tight text-gray-500">
               20GB Cloud storage
             </span>
           </li>
           <li class="flex space-x-3">
             <svg
               aria-hidden="true"
-              class="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
+              class="flex-shrink-0 w-5 h-5 text-blue-600"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -71,14 +71,14 @@ const SubscriptionBox = (props) => {
                 clip-rule="evenodd"
               ></path>
             </svg>
-            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+            <span class="text-base font-normal leading-tight text-gray-500">
               Integration help
             </span>
           </li>
           <li class="flex space-x-3 line-through decoration-gray-500">
             <svg
               aria-hidden="true"
-              class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-gray-500"
+              class="flex-shrink-0 w-5 h-5 text-gray-400"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -97,7 +97,7 @@ const SubscriptionBox = (props) => {
           <li class="flex space-x-3 line-through decoration-gray-500">
             <svg
               aria-hidden="true"
-              class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-gray-500"
+              class="flex-shrink-0 w-5 h-5 text-gray-400"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -116,7 +116,7 @@ const SubscriptionBox = (props) => {
           <li class="flex space-x-3 line-through decoration-gray-500">
             <svg
               aria-hidden="true"
-              class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-gray-500"
+              class="flex-shrink-0 w-5 h-5 text-gray-400"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -135,7 +135,7 @@ const SubscriptionBox = (props) => {
           <li class="flex space-x-3 line-through decoration-gray-500">
             <svg
               aria-hidden="true"
-              class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-gray-500"
+              class="flex-shrink-0 w-5 h-5 text-gray-400"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -154,7 +154,7 @@ const SubscriptionBox = (props) => {
         </ul>
         <button
           type="button"
-          class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+          class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
         >
           Choose plan
         </button>
