6812a29fc185ba0b89cc75bab658214c6eecc1df
nopespace <nopespace2001@gmail.com>
2022-12-03 19:53:12 -0500

    update on subscription

--- PF/tfc/src/components/PagenationBar.jsx
+++ PF/tfc/src/components/PagenationBar.jsx
@@ -6,13 +6,13 @@ import SubscriptionBox from './SubscriptionBox';
 
 function PagenationBar({handlePageClick, pageCount}) {
     return (
-    <ReactPaginate
+    <ReactPaginate className='flex'
         breakLabel="..."
-        nextLabel="next >"
+        nextLabel={<button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Next</button>}
         onPageChange={handlePageClick}
         pageRangeDisplayed={5}
         pageCount={pageCount}
-        previousLabel="< previous"
+        previousLabel={<button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Previous</button>}
         renderOnZeroPageCount={null}
      />)
 }
--- PF/tfc/src/pages/Subscriptions.jsx
+++ PF/tfc/src/pages/Subscriptions.jsx
@@ -4,8 +4,8 @@ import React, { useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
 import { Link } from "react-router-dom";
 import PaginatedItems from "../components/Pagenation";
+import SubscriptionBox from "../components/SubscriptionBox";
 import axios from "axios";
-
 
 const Subscriptions = () => {
   const [details, setDetials] = React.useState([]);
@@ -24,24 +24,24 @@ const Subscriptions = () => {
       .catch((err) => {});
   }, []);
 
-  // useEffect(() => {
-  //   let data;
-  //   axios({
-  //     method: "get",
-  //     url: "http://127.0.0.1:8000/subscriptions/list/",
-  //   })
-  //     .then((res) => {
-  //       data = res.data;
-  //       setDetials(data);
-  //       console.log(data);
-  //     })
-  //     .catch((err) => {});
-  // }, [button]);
-
+  // return (
+  //   <>
+  //     <Navigation />
+  //     <PaginatedItems type = "subscription" items={details} itemsPerPage={2} />
+  //   </>
+  // );
   return (
     <>
       <Navigation />
-      <PaginatedItems type = "subscription" items={details} itemsPerPage={2} />
+      <div className="flex justify-center">
+        {details.map((item) => (
+          <SubscriptionBox
+            key={item.id}
+            billingCycle={item.billing_cycle}
+            price={item.price}
+          />
+        ))}
+      </div>
     </>
   );
 };
