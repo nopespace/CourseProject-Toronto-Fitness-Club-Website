2e1bf9299dd54badf26ee9da5787dc5e4ae7c72a
Simon Chen <simonchen.sc.2002@gmail.com>
2022-12-05 23:14:09 -0500

    📞 Fix all card bugs

--- PF/tfc/src/components/accounts/CardBox.jsx
+++ PF/tfc/src/components/accounts/CardBox.jsx
@@ -70,6 +70,8 @@ const CardBox = (props) => {
                 type="text"
                 name="card_num"
                 id="card_num"
+                minlength="16" 
+                maxlength="16"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 placeholder="Card Number"
                 required
@@ -119,6 +121,8 @@ const CardBox = (props) => {
                 name="CVV"
                 id="CVV"
                 placeholder="123"
+                minlength="3" 
+                maxlength="3"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 required
               />
